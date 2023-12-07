7f393940dc3f9df805b664c5fc5e280eea7c3071
Madderstone <anthony.issa@hotmail.fr>
2023-12-07 10:12:02 +0100

    done

--- index.js
+++ index.js
@@ -1,11 +1,78 @@
 const { default: axios } = require("axios");
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 const baseUrl = "https://api.binance.com";
+
+const db = new sqlite3.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`CREATE TABLE candles (
+    Id INTEGER PRIMARY KEY,
+    date INT, high REAL, low REAL, open REAL, close REAL, volume REAL)`);
+
+  db.run(`CREATE TABLE full_data (
+    Id INTEGER PRIMARY KEY,
+    uuid TEXT, traded_crypto REAL, price REAL,
+    created_at_int INT, side TEXT)`);
+
+  db.run(`CREATE TABLE last_checks (
+    Id INTEGER PRIMARY KEY,
+    exchange TEXT, trading_pair TEXT, duration TEXT,
+    table_name TEXT, last_check INT, startdate INT, last_id INT)`);
+});
+
+const updateCandleData = (data) => {
+  const stmt =
+    db.prepare(`INSERT INTO candles (date, high, low, open, close, volume)
+                           VALUES (?, ?, ?, ?, ?, ?)`);
+  data.forEach((candle) => {
+    stmt.run(candle[0], candle[2], candle[3], candle[1], candle[4], candle[5]);
+  });
+  stmt.finalize();
+};
+
+const storeTradeData = (data) => {
+  const stmt =
+    db.prepare(`INSERT INTO full_data (uuid, traded_crypto, price, created_at_int, side)
+                           VALUES (?, ?, ?, ?, ?)`);
+  data.forEach((trade) => {
+    stmt.run(
+      trade.uuid,
+      trade.traded_crypto,
+      trade.price,
+      trade.created_at_int,
+      trade.side
+    );
+  });
+  stmt.finalize();
+};
+
+const updateLastCheck = (
+  exchange,
+  tradingPair,
+  duration,
+  tableName,
+  lastCheck,
+  startDate,
+  lastId
+) => {
+  db.run(
+    `INSERT INTO last_checks (exchange, trading_pair, duration, table_name, last_check, startdate, last_id)
+          VALUES (?, ?, ?, ?, ?, ?, ?)`,
+    exchange,
+    tradingPair,
+    duration,
+    tableName,
+    lastCheck,
+    startDate,
+    lastId
+  );
+};
 
 app.get("/tickers", async (req, res) => {
   try {
@@ -56,6 +123,19 @@ app.get("/candles", async (req, res) => {
     const endTime = Date.now();
     const url = `${baseUrl}/api/v3/uiKlines?symbol=${symbol}&interval=1m&startTime=${startTime}&endTime=${endTime}`;
     const response = await axios.get(url);
+
+    updateCandleData(response.data);
+
+    updateLastCheck(
+      "Binance",
+      symbol,
+      duration,
+      "candles",
+      Date.now(),
+      startTime,
+      null
+    );
+
     res.send(response.data);
   } catch (error) {
     res.status(400).send(error.response.data);
@@ -67,6 +147,9 @@ app.get("/trades", async (req, res) => {
     const { symbol } = req.query;
     const url = `${baseUrl}/api/v3/trades?symbol=${symbol}`;
     const response = await axios.get(url);
+
+    storeTradeData(response.data);
+
     res.send(response.data);
   } catch (error) {
     res.status(400).send(error.response.data);
